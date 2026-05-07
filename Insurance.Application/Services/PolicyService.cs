ea69b7b030b16536eeae53c3a4402c9d50521afa
Anushri Awari <awarianushri@gmail.com>
2026-05-07 23:16:46 +0530

    completed policy module changes

--- Insurance.Application/Services/PolicyService.cs
+++ Insurance.Application/Services/PolicyService.cs
@@ -1,4 +1,4 @@
-﻿using Insurance.Application.Interfaces;
+﻿ using Insurance.Application.Interfaces;
 using Insurance.Domain.Interfaces;
 using Insurance.Domain.Models;
 
@@ -12,19 +12,49 @@ public class PolicyService : IPolicyService
 		_repository = repository;
 	}
 
-    
-    
     public async Task<IEnumerable<Policy>> GetPoliciesAsync()
     {
-        return await _repository.GetAllPoliciesAsync();
+        try
+        {
+            var policies = await _repository.GetAllPoliciesAsync();
+
+            // If the list is empty, that's not a crash, but we can handle it
+            if (policies == null || !policies.Any())
+            {
+                // We can return an empty list or throw a specific error
+                return Enumerable.Empty<Policy>();
+            }
+            return policies;
+        }
+
+        catch (Exception ex)
+        {
+            throw;
+        }
     }
 
     public async Task<Policy> GetPolicyByIdAsync(int id)
     {
-        return await _repository.GetPolicyByIdAsync(id);
+
+        if (id < 0)
+        {
+            throw new ArgumentException("Id Invalid");
+        }
+        var policy = await _repository.GetPolicyByIdAsync(id);
+
+        if (policy == null)
+        {
+            throw new KeyNotFoundException($"No policy with {id}");
+        }
+        return policy;
     }
+
     public async Task AddPolicyAsync(Policy policy)
     {
+        if (policy == null)
+        {
+            throw new ArgumentNullException(nameof(policy), "Policy data cannot be null.");
+        }
         if (policy.PremiumAmount <= 0)
         {
             throw new Exception("Premium Amount cannot be 0");
